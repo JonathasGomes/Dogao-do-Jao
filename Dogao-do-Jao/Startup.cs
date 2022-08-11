d86179ed64475b4cab123739af47872625e5fee3
JonathasGomes <jonathas553@hotmail.com>
2022-08-11 18:31:04 -0300

    Initial Commit

--- Dogao-do-Jao/Startup.cs
+++ Dogao-do-Jao/Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace Dogao_do_Jao;
+﻿using Dogao_do_Jao.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dogao_do_Jao;
 
     public class Startup
     {
@@ -12,6 +15,9 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
