d594a0ea55f86483820d89deeee11040fe1c9882
JonathasGomes <jonathas553@hotmail.com>
2022-09-16 09:51:34 -0300

    LancheListViewModel implementada

--- Dogao-do-Jao/Controllers/LancheController.cs
+++ Dogao-do-Jao/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using Dogao_do_Jao.Repositories.Interfaces;
+using Dogao_do_Jao.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dogao_do_Jao.Controllers
@@ -14,16 +15,22 @@ namespace Dogao_do_Jao.Controllers
 
         public IActionResult List()
         {
-            ViewData["Titulo"] = "Todos os lanches";
-            ViewData["Data"] = DateTime.Now;
+            //ViewData["Titulo"] = "Todos os lanches";
+            //ViewData["Data"] = DateTime.Now;
 
-            var lanches = _lancheRepository.Lanches;
-            var totalLanches = lanches.Count();
+            //var lanches = _lancheRepository.Lanches;
+            //var totalLanches = lanches.Count();
 
-            ViewBag.Total = "Total de lanches : ";
-            ViewBag.TotalLanches = totalLanches;
+            //ViewBag.Total = "Total de lanches : ";
+            //ViewBag.TotalLanches = totalLanches;
 
-            return View(lanches);
+            //return View(lanches);
+
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lanchesListViewModel);
         }
     }
 }
---
+++ Dogao-do-Jao/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Dogao_do_Jao.Models;
+
+namespace Dogao_do_Jao.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
